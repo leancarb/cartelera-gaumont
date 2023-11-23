13b3fbbb133768e7ace26ac8b267342d2163cff1
Leandro <leancarbonetti@gmail.com>
2023-11-23 18:25:29 -0300

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,9 @@
 const root = document.querySelector("#root");
+
+window.addEventListener("load", function () {
+  var link = document.createElement("link");
+  link.href =
+    "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap";
+  link.rel = "stylesheet";
+  document.head.appendChild(link);
+});
